adf808e667467f1d13ef2a87c5fcb02d368c2bdd
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2023-06-20 15:18:36 +0500

    Image Files Base64

--- sayts/pagination.py
+++ sayts/pagination.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class LargeResultsSetPagination(PageNumberPagination):
-    page_size = 2
+    page_size = 10
     page_size_query_param = '1'
     max_page_size = 100
